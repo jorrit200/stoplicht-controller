33314c7df68f8a587c975344dfe8fb5771ef5767
ttttullll <114577122+tullllllll@users.noreply.github.com>
2025-02-12 14:23:47 +0100

    Empty project
    Created spring project with websockets

---
+++ stoplicht_controller/src/main/java/com/stoplicht_controller/stoplicht_controller/StoplichtControllerApplication.java
@@ -0,0 +1,13 @@
+package com.stoplicht_controller.stoplicht_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoplichtControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoplichtControllerApplication.class, args);
+    }
+
+}
---
+++ stoplicht_controller/src/test/java/com/stoplicht_controller/stoplicht_controller/StoplichtControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoplicht_controller.stoplicht_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoplichtControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
