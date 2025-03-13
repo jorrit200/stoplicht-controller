a17a1fc5a2235c248a83d3deeb44bc2ff0aec686
ttttullll <114577122+tullllllll@users.noreply.github.com>
2025-03-13 17:02:04 +0100

    fixed enums

--- stoplicht_controller/src/main/java/com/stoplicht_controller/stoplicht_controller/Entities/TrafficLights.java
+++ stoplicht_controller/src/main/java/com/stoplicht_controller/stoplicht_controller/Entities/TrafficLights.java
@@ -4,9 +4,7 @@ import Config.TrafficLightConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stoplicht_controller.stoplicht_controller.Enums.TrafficlightState;
 import com.stoplicht_controller.stoplicht_controller.Util.JsonReader;
-import org.apache.catalina.webresources.StandardRoot;
 import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Service;
 import java.util.Dictionary;
 import java.util.Hashtable;
@@ -18,12 +16,12 @@ public class TrafficLights {
     public TrafficLights() {
         TrafficLightConfig trafficLightConfigFromSpec = JsonReader.getTrafficLightConfigFromSpec();
         trafficLightConfigFromSpec.getGroups().keySet()
-                .forEach(groupKey -> trafficLights.put(groupKey, TrafficlightState.ROOD));
+                .forEach(groupKey -> trafficLights.put(groupKey, TrafficlightState.rood));
     }
 
     public String fillTrafficLights() throws JSONException {
         try {
-            trafficLights.put(1, TrafficlightState.GROEN); // Voeg de waarde toe
+            trafficLights.put(1, TrafficlightState.groen); // Voeg de waarde toe
 
             ObjectMapper objectMapper = new ObjectMapper();
             String json = objectMapper.writeValueAsString(trafficLights); // Serialiseer de hele map
